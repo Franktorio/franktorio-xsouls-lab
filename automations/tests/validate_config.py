c5cdb052d9095000143468fc08263ef8cd7ee8cb
FranktorioServer <lordofroblaxyt@gmail.com>
2025-12-15 07:04:53 +0000

    Used exec() to build an iterable dictionary to allow for comparing the real vars.py against the example.

--- automations/tests/validate_config.py
+++ automations/tests/validate_config.py
@@ -10,23 +10,29 @@
 # Make sure have vars.py file in config/ directory before running this script.
 # Use vars.py.example as a template.
 
+
+
 PRINT_PREFIX = "CONFIG VALIDATION"
 
 
 try:
     from config import vars as config_vars
+    with open("config/vars.py.example", "r") as example_file:
+        example_content = example_file.read()
+    to_check = {}
+    exec(example_content, to_check)
 except ImportError as e:
-    print(f"Failed to import configuration module: {e}")
-    print("Please ensure that 'config/vars.py' exists and is correctly formatted.")
+    print(f"[WARNING] [{PRINT_PREFIX}] Failed to import configuration module: {e}")
+    print(f"[WARNING] [{PRINT_PREFIX}] Please ensure that 'config/vars.py' exists and is correctly formatted.")
     exit(1)
 
 not_set = []
-for name, value in config_vars.__dict__.items():
-    print(f"[DEBUG] [{PRINT_PREFIX}] Checking configuration variable: {name}")
-    if name.isupper():
-        if value is None or (isinstance(value, str) and value.startswith("YOUR_")):
-            print(f"[WARNING] [{PRINT_PREFIX}] Configuration variable '{name}' is not set.")
-            not_set.append(name)
+for key in to_check:
+    if key.isupper():  # Only check uppercase variables
+        if not hasattr(config_vars, key): # Variable not set
+            not_set.append(key)
+        elif getattr(config_vars, key) == to_check[key] and type(getattr(config_vars, key)) == str: # Variable set to default/example value as string
+            not_set.append(key)
 
 if not not_set:
     print(f"[INFO] [{PRINT_PREFIX}] All configuration variables are set correctly.")
